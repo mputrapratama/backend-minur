96fb714a68ef7fa69c60a4d96176cf19ecfd889c
renn18 <ppskdos>
2022-09-06 13:37:48 +0800

    progress project hari ke-9

--- crud/backend/controllers/UserController.js
+++ crud/backend/controllers/UserController.js
@@ -45,10 +45,53 @@ export const createUsers = async(req, res) => {
     }
 }
 
-export const updateUsers = (req, res) => {
-
+export const updateUsers = async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    const {name, email, password, confPassword, role} = req.body;
+    let hashPassword;
+    if (password === "" || password === null){
+        hashPassword = user.password
+    }else{
+        hashPassword = await argon2.hash(password);
+    }
+    if (password !== confPassword) return res.status(400).json({msg: "Password dan Confirm Password tidak cocok"});
+    try {
+        await User.update({
+            name: name,
+            email: email,
+            password: hashPassword,
+            role: role
+        },{
+            where:{
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "User Updated"});
+    } catch (error) {
+        res.status(900).json({msg: error.message});
+    }
 }
 
-export const deleteUsers = (req, res) => {
-
+export const deleteUsers = async (req, res) => {
+    const user = await User.findOne({
+        where: {
+            uuid: req.params.id
+        }
+    });
+    if(!user) return res.status(404).json({msg: "User tidak ditemukan"});
+    try {
+        await User.destroy({
+            where: {
+                id: user.id
+            }
+        });
+        res.status(200).json({msg: "User deleted"});
+    } catch (error) {
+        res.status(400).json({msg: error.message});
+    }
 }
--- crud/backend/index.js
+++ crud/backend/index.js
@@ -2,15 +2,23 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 
-// import LoginRoute from "./routes/LoginRoute.js";
-// import UserRoute from "./routes/UserRoute.js";
-// import ContentRoute from "./routes/ContentRoute.js";
+import LoginRoute from "./routes/LoginRoute.js";
+import UserRoute from "./routes/UserRoute.js";
+import ContentRoute from "./routes/ContentRoute.js";
 
-// import db from "./config/database.js";
+import db from "./config/database.js";
+import SequelizeStore from "connect-session-sequelize";
 import dotenv from "dotenv";
+import AuthRoute from "./routes/AuthRoute.js";
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db:db
+});
 
 // (async()=>{
 //     await db.sync();
@@ -20,6 +28,7 @@ app.use(session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized:true,
+    store: store,
     cookie: {
         secure: 'auto'
     }
@@ -31,9 +40,13 @@ app.use(cors({
 }));
 app.use(express.json());
 
-// app.use(LoginRoute);
-// app.use(UserRoute);
-// app.use(ContentRoute);
+app.use(LoginRoute);
+app.use(UserRoute);
+app.use(ContentRoute);
+app.use(AuthRoute);
+
+// store.sync();
+
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log('Server up and running ...');
